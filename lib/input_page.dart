f31886ff1b7011ee853bb4fc2cc5dffa8ed1a027
GhayoorUlHaq <ghayoor10th@gmail.com>
2021-01-24 21:50:57 +0500

    constant defined

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+const activeBackgroundColor = Color(0xFF1d1E33);
+const bottomContainerColor = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -9,50 +13,56 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('BMI Calculator'),
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: Row(
-                children: [
-                  Expanded(
-                    child: ReusableCard(
-                      colour: Color(0xFF1d1E33),
-                    ),
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeBackgroundColor,
                   ),
-                  Expanded(
-                    child: ReusableCard(
-                      colour: Color(0xFF1d1E33),
-                    ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeBackgroundColor,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Expanded(
-              child: ReusableCard(
-                colour: Color(0xFF1d1E33),
-              ),
+          ),
+          Expanded(
+            child: ReusableCard(
+              colour: activeBackgroundColor,
             ),
-            Expanded(
-              child: Row(
-                children: [
-                  Expanded(
-                    child: ReusableCard(
-                      colour: Color(0xFF1d1E33),
-                    ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeBackgroundColor,
                   ),
-                  Expanded(
-                    child: ReusableCard(
-                      colour: Color(0xFF1d1E33),
-                    ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeBackgroundColor,
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ));
+          ),
+          Container(
+            color: bottomContainerColor,
+            width: double.infinity,
+            height: bottomContainerHeight,
+          ),
+        ],
+      ),
+    );
   }
 }
 
